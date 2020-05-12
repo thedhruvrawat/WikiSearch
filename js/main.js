d73d74a9db86aebefa895db186b3b1688c300806
Dhruv Rawat <dhruv.noida175@gmail.com>
2020-05-12 07:29:08 +0530

    overhaul

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ window.onload = function() {
 
 function ajax (keyword) { //AJAX request
 	$.ajax({ 
-		url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
+		url: "//en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
 		dataType: "jsonp",
 		success: function(response) {
-			console.log(response.query);
+			// console.log(response.query);
 			if (response.query.searchinfo.totalhits === 0) {
 				showError(keyword);
 			}
@@ -25,16 +25,58 @@ function ajax (keyword) { //AJAX request
 function showResults (callback) {
 
 	for (var i = 0; i <= 9; i++) {
-		$(".display-results").append("<div class='result-list result-" + i + "'>" + "<span class='result-title title-" + i + "'></span>" + "<br>" +"<span class='result-snippet snippet-" + i + "'></span>" + "<br>" + "<span class='result-metadata metadata-" + i + "'></span>" + "</div>" );
+		// $(".display-results").append("<div class='card mb-3 result-list result-" + i + "' style='max-width: 540px;'>" + "<span class='card-img-top img-" + i + "'></span>" + "<span class='result-title title-" + i + "'></span>" + "<br>" +"<span class='result-snippet snippet-" + i + "'></span>" + "<br>" + "<span class='result-metadata metadata-" + i + "'></span>" + "</div>" );
+
+		$(".display-results").append("<div class='card mb-5 mycard result-" + i + "' >" + "<div class='row no-gutters'>" +
+			"<div class='col-md-4'>" + "<div class='card-img img-" + i + "'></div>" + "</div>" + 
+			"<div class='col-md-8'>" + 
+			"<div class='card-body'>" +
+				"<h5 class='card-title title-" + i + "'></h5>" + 
+				"<p class='card-text snippet-" + i + "'></p>" +
+				"<p class='card-text'><small class='text-muted metadata-" + i + "'></small></p>" + 
+			"</div></div></div></div>");
+
+
+
+
 	}
 
 	for (var m = 0; m <= 9; m++) {
+		var id = callback.query.search[m].pageid;
+		// console.log(id);
 		var title = callback.query.search[m].title;
 		var url = title.replace(/ /g, "_");
+		var imggetter = `//en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=${url}&piprop=thumbnail&pithumbsize=250&pilimit=20`;
+		$.ajax({
+			url: imggetter,
+			method: "GET",
+			dataType: "jsonp",
+			ajaxID: id,
+			ajaxM: m,
+			success: function(data) {
+				var newid = this.ajaxID;
+				m = this.ajaxM;
+				// console.log(newid);
+
+				if (data.query.pages[newid].hasOwnProperty("thumbnail") === true) {
+					$(".img-" + m).html("<img width=100% height=100% src='"+`${data.query.pages[newid].thumbnail.source}`+"'>");
+
+					// $(".img-" + m).css('background', 'red !important');
+					// .style.background = "url(" + `${data.query.pages[newid].thumbnail.source}` +") !important";
+					// ("<img src='" + `${data.query.pages[newid].thumbnail.source}` + "'>");
+				  } else {
+					$(".img-" + m).html("<img width=50% src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Article_icon_cropped.svg/512px-Article_icon_cropped.svg.png'>");
+				  }
+
+				}
+		});
+		
+		
+		
 		var timestamp = callback.query.search[m].timestamp;
 		timestamp = new Date(timestamp);
-		console.log(timestamp);
-		$(".title-" + m).html("<a href='https://en.wikipedia.org/wiki/" + url + "' target='_blank'>" + callback.query.search[m].title + "</a>");
+		// console.log(imggetter.query.pages);
+		$(".title-" + m).html("<a href='//en.wikipedia.org/wiki/" + url + "' target='_blank'>" + callback.query.search[m].title + "</a>");
 		$(".snippet-" + m).html(callback.query.search[m].snippet);
 		$(".metadata-" + m).html((callback.query.search[m].size/1000).toFixed(0) + "kb (" + callback.query.search[m].wordcount + " words) - " + timestamp);
 	}
